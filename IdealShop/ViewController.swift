99a0f0e4cbc6bba04c010d09ca52698de52c9be4
Vladimir <motiw@icloud.com>
2023-03-11 22:08:46 +0600

    Initial Commit

---
+++ IdealShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdealShop
+//
+//  Created by Vladimir on 11.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
